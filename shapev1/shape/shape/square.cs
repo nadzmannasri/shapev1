da33233459396a65b79831f7c86c2f6c77ee0f66
nadzmannasri <116859531+nadzmannasri@users.noreply.github.com>
2022-11-04 11:44:49 +0800

    Add files via upload

---
+++ shapev1/shape/shape/square.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    class square : rectangle
+    {
+        public new double Width
+        {
+            get { return base.Width; }
+            set 
+            { 
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public new double Height
+        {
+            get { return base.Height; }
+            set 
+            { 
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        public double Len
+        {
+            get { return base.Height; }
+            set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        public square() : base()
+        {
+            
+        }
+
+        public square(double len): base(len,len)
+        {
+
+        }
+
+    }
+}
